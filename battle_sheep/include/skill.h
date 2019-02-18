21904b6b55abb226761d8f61ee33ecbd0a916a12
gkusoglu <gkusoglu@etu.unistra.fr>
2019-02-18 18:29:55 +0100

    get and set skill

--- battle_sheep/include/skill.h
+++ battle_sheep/include/skill.h
@@ -8,15 +8,23 @@ class Skill
     /*Methods*/
   public:
     //Constructor :
-    Skill(bool unlimited_a, bool used_a);
+    Skill(bool unlimited_a, bool used_a, int x_a, int y_a, int a_a, int b_a);
 
     //Getters :
     bool is_unlimited();
     bool is_used();
+    int get_x_location();
+    int get_y_location();
+    int get_a_location();
+    int get_b_location();
 
     //Setters :
     void set_unlimited(bool unlimited_a);
     void set_used(bool used_a);
+    void set_x_location(int x_a);
+    void set_y_location(int y_a);
+    void set_a_location(int a_a);
+    void set_b_location(int b_a);
 
     //Others :
     /* Description :
@@ -28,6 +36,11 @@ class Skill
   protected:
     bool unlimited;
     bool used;
+    int x;
+    int y;
+    int a;
+    int b;
+    Square *destination_grid[10][10];
 };
 
 #endif
--- battle_sheep/src/skill.cpp
+++ battle_sheep/src/skill.cpp
@@ -2,6 +2,50 @@
 
 using namespace std;
 
-Skill::Skill(){
-    
+Skill::Skill(bool unlimited_a, bool used_a, int x_a, int y_a, int a_a, int b_a)
+    : unlimited(unlimited_a),
+      used(used_a),
+      x(x_a),
+      y(y_a),
+      a(a_a),
+      b(b_a)
+{
+}
+bool Skill::is_unlimited(){
+    return unlimited;
+}
+bool Skill::is_used(){
+    return used;
+}
+int Skill::get_x_location(){
+    return x;
+}
+int Skill::get_y_location(){
+    return y;
+}
+int Skill::get_a_location(){
+    return a;
+}
+int Skill::get_b_location(){
+    return b;
+}
+
+//Setters :
+void Skill::set_unlimited(bool unlimited_a){
+    unlimited=unlimited_a;
+}
+void Skill::set_used(bool used_a){
+    used=used_a;
+}
+void Skill::set_x_location(int x_a){
+    x=x_a;
+}
+void Skill::set_y_location(int y_a){
+    y=y_a;
+}
+void Skill::set_a_location(int a_a){
+    a=a_a;
+}
+void Skill::set_b_location(int b_a){
+    b=b_a;
 }
