2a27ace5464c7879aef841fdb08d0eb7da89e857
@keppel <arnaud.keppel@etu.unistra.fr>
2019-02-18 16:51:13 +0100

    Getter & setter Square

--- battle_sheep/include/square.h
+++ battle_sheep/include/square.h
@@ -12,9 +12,13 @@ class Square
 
     //Getters :
     Engine *get_engine_here();
+    int get_depth();
+    char get_hazard();
 
     //Setters :
     void set_engine_here(Engine *engine_here_a);
+    void set_depth(int nv);
+    void set_hazard(char nv);
 
     /*Attributes*/
   private:
@@ -22,9 +26,6 @@ class Square
     Engine *engine_here;
     int depth;
     char hazard; //climate changes (c : cold, t : tempest, n : none ... ?)
-
-
-
 
 };
 
