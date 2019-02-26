5e85112f9c2789ac94659e6f8c406809239b2cb8
KNAPP LIESSE <liesse.knapp@etu.unistra.fr>
2019-02-26 17:31:43 +0100

    add Engine::proximity_check()

--- battle_sheep/include/hub.h
+++ battle_sheep/include/hub.h
@@ -16,17 +16,7 @@ class Hub
     //Getters :
 
     //Setters :
-
-    //Others :
-    /* Description : Check that all the squares around a segment are ok.
-    Input : a grid number to "fire" it, (x,y) where skill begin and (x,y) where
-            it end.
-    Output : - true, there is something around;
-             - false, nothing is around. */
-    bool proximity_check(Square *grid[10][10], int x_starting_location,
-                         int y_starting_location, int x_endind_location,
-                         int y_ending_location);
-
+    
     /*Attributes*/
   private:
     Square *square_tab[4][10][10];
