e2932ef2cff99eea0f3b355eda01349587fec05c
stevlulz <stevlulz@localhost.localdomain>
2019-03-06 22:26:54 +0100

    FactorySkill,Skill Headers Added

---
+++ battle_sheep/include/FactoySkill.h
@@ -0,0 +1,24 @@
+//
+// Created by stevlulz on 3/6/19.
+//
+
+#ifndef HAZARDOUS_FACTOYSKILL_H
+#define HAZARDOUS_FACTOYSKILL_H
+
+#include "enum.h"
+#include "Skill.h"
+class FactoySkill {
+
+    private:
+        int m_points;
+        ENGINE_TYPE m_engine_type;
+    public:
+    /*
+     * Calling on this method will decrease the m_points by certain point...don't call it if you're not sure !
+     */
+    Skill getSkill(SHOT_TYPE );
+
+};
+
+
+#endif //HAZARDOUS_FACTOYSKILL_H
