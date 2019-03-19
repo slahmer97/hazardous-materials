c1a5a3bd3e6efd615963a2978a1d7d587e76afaa
MASSART JULES <jules.massart@etu.unistra.fr>
2019-03-19 16:14:34 +0100

    Comment next Grid.h

--- battle_sheep/include/Grid.h
+++ battle_sheep/include/Grid.h
@@ -69,23 +69,60 @@ public:
     /* Input : int length x,y(position), bool IEM,horizontal, float damage
     * Shot or Desactivate (IEM) on a line from the posistion (x,y) for length with the 
     * orientation (horizontal) and make a damage (for each Square in the trajectory)
-    * Output : Number of the case hit
+    * Output : Number of cases hit
     */
     int line_shot(int x, int y, int length, bool IEM, bool horizontal, float damage = 1.f);
 
+    /* Input : int length x,y(position), bool IEM, float damage
+    * Shot or Desactivate (IEM) on a vertical line from the posistion (x,y) for 
+    * length and make a damage (for each Square in the trajectory)
+    * Output : Number of cases hit
+    */
     int vertical_line_shot(int x, int y, int length, bool IEM, float damage = 1.f);
 
+    /* Input : int length x,y(position), bool IEM, float damage
+    * Shot or Desactivate (IEM) on a horizontal line from the posistion (x,y) for 
+    * length and make a damage (for each Square in the trajectory)
+    * Output : Number of cases hit
+    */
     int horizontal_line_shot(int x, int y, int length, bool IEM, float damage = 1.f);
 
+    /* Input : int length,large x,y(position), bool IEM, float damage
+    * Shot or Desactivate (IEM) on a recantgular area of dimension large (vertical) 
+    * and length (horizontal) from the posistion (x,y) and make a damage (for 
+    * each Square in the trajectory)
+    * Output : Number of cases hit
+    */
     int rectangular_shot(int x, int y, int large, int length, bool IEM, float damage = 1.f);
 
+    /* Input : int length x,y(position), bool IEM, float damage
+    * Shot or Desactivate (IEM) a Square at the position (x,y) and make the same
+    * for each neighbor (in the 4 directions), number of neighbor in one 
+    * direction (length)
+    * Output : Number of cases hit
+    */
     int cross_shot(int x, int y, int length, bool IEM, float damage = 1.f);
 
+    /* Input : int length x,y(position), bool IEM,horizontal float damage
+    * Shot or Desactivate (IEM) each Square meet from the position(x,y) in the
+    * direction horizontal until find an engine (or end of the line) and after 
+    * hit the next Square (no matter if there is something or not)
+    * Output : Number of cases hit (0, 1 or 2)
+    */
     int first_to_drawn(int x, int y, bool horizontal, bool IEM, float damage = 1.f);
 
+    /* Input : int x,y (position)
+    * Check if the position (x,y) is in the grid
+    * Ouput : 1 yes, -1 no
+    */
+    int check_one_position(int x, int y);
+    
+    /* Input : Engine, int x,y (position)
+    * Check if there is nothing at this position
+    * Output : 1 nothing, -1 something or -100 is not an engine
+    */
     int proximity_check(int x, int y, Engine *engine);
 
-    int check_one_position(int x, int y);
 
     Square* radar_one_square(int x, int y);
 
