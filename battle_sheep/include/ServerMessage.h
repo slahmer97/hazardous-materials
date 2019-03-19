48de6586823c8e347a484963b10b06a2ceee2c83
sidahmedlahmer <seyyidahmed.lahmer@etu.unistra.fr>
2019-03-19 17:27:17 +0100

    prototype function added ServerMessage.h
    Signed-off-by: sidahmedlahmer <seyyidahmed.lahmer@etu.unistra.fr>

--- battle_sheep/include/ServerMessage.h
+++ battle_sheep/include/ServerMessage.h
@@ -5,14 +5,41 @@
 #ifndef HAZARDOUS_MATERIALS_SERVERMESSAGE_H
 #define HAZARDOUS_MATERIALS_SERVERMESSAGE_H
 
+#include "enum.h"
+
 namespace network{
 class ServerMessage {
 
 
-
+public:
     enum SERVER_MESSAGE_TYPE{
         KILL_PLAYER,GRIDS_ASSIGNEMENT,ERROR,CHAT_S,CURRENT_TURN,SCORE_BROADCAST
     };
+    enum SKILL_TYPE{
+        SKILLED,NORMAL
+    };
+    enum ERRORS{BLABLA};
+
+
+    static std::string getKillPlayerMessage(unsigned char id_player_grid);
+    static std::string getGridAssinementMessage(std::string username, unsigned char id_grid);
+    static std::string getErrorMessage(ERRORS error,std::string clientMessage);//client message to be modified into CLIENT_MESSAGE_TYPE
+    static std::string getChatMessage(std::string message,unsigned char id);
+    static std::string getCurrentTurnMessage(unsigned char id);
+    static std::string getScoreBroadCastMessage(std::string score);
+
+
+private:
+    SERVER_MESSAGE_TYPE m_msg_type;
+    unsigned char m_id;
+    std::string m_grid;
+    std::string m_chat_msg;
+    std::string m_username;
+    ERRORS m_error;
+    std::string score;
+   // CLIENT_MESSAGE
+
+
 
 };
 
