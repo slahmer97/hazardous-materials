022d2460d6ae42f78ddec87d92f8840c8f30b789
sidahmed <seyyidahmed.lahmer@etu.unistra.fr>
2019-04-23 15:48:49 +0200

    delete blabla

--- battle_sheep/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arnaud/hazardous-materials/battle_sheep
-# Build directory: /home/arnaud/hazardous-materials/battle_sheep
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runUnitTests "runUnitTests")
