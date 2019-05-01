e66389b36fb9e8bdf01aaf24c67eb9c463fc3235
Alexandre MEYER <alexandre.meyer3@etu.unistra.fr>
2019-05-01 14:12:14 +0200

    Ajout fichier square.{h,cpp}

--- battle_sheep/include/square.h
+++ battle_sheep/include/square.h
@@ -1,36 +1,131 @@
-#ifndef SQUARE
-#define SQUARE
+//
+// Created by stevlulz on 3/7/19.
+//
 
-#include "engine.h"
+#ifndef TMP_PROJ_SQUARE_H
+#define TMP_PROJ_SQUARE_H
+
+
+#include "Engine.h"
+#include "enum.h"
 
 class Engine;
-
 class Square
 {
 
 private:
+    Engine *m_engine;
+    SQUARE_TYPE m_square_type;
+    float m_health_pr;
+    //int depth;
+    //char hazard; //climate changes (c : cold, t : tempest, n : none ... ?)
 
-Engine *engine_here;
-int depth;
-char hazard;     //climate changes (c : cold, t : tempest, n : none ... ?)
-bool engine_dead;
 public:
+    Square();
 
-Square();
-//Square(Engine engine_here_nv, int depth_nv, char hazard_nv);
+    void set_engine(Engine *m_engine);
 
-Engine *get_engine_here();
-int get_depth();
-char get_hazard();
-bool get_engine_death();
 
-void set_engine_here(Engine *engine_here_a);
-void set_depth(int nv);
-void set_hazard(char nv);
-void set_engine_death(bool engine_dead_a);
 
-void get_infos();
+    /**
+     * @brief get_engine is main function used to check if a square has an engine
+     *        residing in it or no
+     * @return return a reference for engine which is residing in this square ?
+     *         nullptr if square is empty
+     */
+    Engine *get_engine();
+
+
+    /**
+     * @brief get_health_pr() is main function used to get the pourcetage of health of
+     *        the engine's part residing on it
+     * @return 0 if the engine part residing on this square is dead or square is empty ?
+     *         otherwise it returns the remainning health pourcentage (positive float) for current part
+     */
+    float get_health_pr();
+
+
+
+    /**
+     * @brief desactivate_motor() is main function used to desactivate engine's motor which reside
+     * on current square if the current square has an avtivated motor of an engine, it desactivates it otherwise it does nothing
+     */
+    void desactivate_motor();
+
+
+
+    /**
+     * @brief desactivate_weapon() is main function used to desactivate engine's weapon which reside
+     * on current square if the current square has an activated weapon of an engine, it desactivates it otherwise it does nothing
+    */
+    void desactivate_weapon();
+
+
+
+    /**
+     * @brief activate_weapon() is main function used to reasactivate engine's weapon which reside
+     * on current square if the current square has a desactivated weapon of an engine, it reactivates it otherwise it does nothing
+     */
+    void activate_weapon();
+
+
+    /**
+     * @brief activate_weapon() is main function used to reasactivate engine's motor which reside
+     * on current square if the current square has a desactivated motor of an engine, it reactivates it otherwise it does nothing
+    */
+    void activate_motor();
+
+
+
+    /**
+     * @brief get_square_type() is main function used to get the state of current square or type of piece which is
+     *        residing on it(motor,nothing,....etc)
+     * @return
+     *         NONE : current square is empty
+     *         ENGINE_MOTOR : current square contains an engine's motor
+     *         ENGINE_MOTOR_DESACTIVATED : current square contains a desactivated motor
+     *         ENGINE_WEAPON : current square constrains an engine's weapon
+     *         ENGINE_WEAPON_DESACTIVATED : current square contains
+     *         ENGINE_PART : current square contains other engine's part(except motor and weapons)
+     *         ENGINE_DEAD : current square contrains engine's crumbs
+     */
+    SQUARE_TYPE get_square_type();
+
+    /**
+     * @brief decrease_health() is the only function that has access to decrease the health of current engine's part
+     *        -if the current square has no engine's piece on it, then it does nothing (it just returns)
+     *
+     *        -if the current square has an engine's part, then it decreases the health of current square
+     *         (if it arrives to 0 it made the square as ENGINE_DEAD),then decreases the health of total
+     *         health of engine residing on it.
+     *
+     *        -if the current square has an engine's weapon, then it does same as previous (if it arrives to 0
+     *          TODO (maybe desactivate weapon)
+     *
+     *         -if the current square has an engine's motor, then it does same as previous (if it arrives to 0
+     *         TODO (maybe desactivate the movements)
+     *
+     * @param dec the value of decreasing the current square's health and total engine's health
+     * @return
+     *         - if -1 then the current square doesn't contrain any engine part
+     *         - (TODO other cases)
+     */
+    int decrease_health(float dec);
+    int increase_health(float inc);
+
+    void set_health_pr(float pr);
+
+
+
+    /**
+     * @brief set_square_type() this is main function that has permission to modify the current square's type
+     *        check enum.h(SQUARE_TYPE)
+     * @param square_type the value of current's square type
+     */
+    void set_square_type(SQUARE_TYPE square_type);
 
 };
 
-#endif
+
+
+#endif //TMP_PROJ_SQUARE_H
--- battle_sheep/src/square.cpp
+++ battle_sheep/src/square.cpp
@@ -1,106 +1,99 @@
-#include "../include/square.h"
-#include <iostream>
+//
+// Created by stevlulz on 3/7/19.
+//
+#include "../include/Square.h"
 
-/*indentation by uncrustify
-   https://github.com/uncrustify/uncrustify */
 
-using namespace std;
 
-Square::Square() // no arguments
-{
-        depth = 1;
-        hazard = 'n'; //n for neutral
-        engine_here = NULL;
-        engine_dead = false;
+Square::Square(){
+    m_engine = nullptr;
+    m_health_pr = .0f;
+    m_square_type = SQUARE_TYPE::NONE;
 }
 
-Engine *Square::get_engine_here()
-{
-        return this->engine_here;
+Engine *Square::get_engine(){
+    return m_engine;
 }
-bool Square::get_engine_death()
-{
-	return this->engine_dead;
+float Square::get_health_pr(){
+    return m_health_pr;
+}
+void Square::desactivate_motor(){
+    if(m_square_type == SQUARE_TYPE::ENGINE_MOTOR)
+        m_square_type = SQUARE_TYPE::ENGINE_MOTOR_DESACTIVATED;
+}
+void Square::desactivate_weapon(){
+    if(m_square_type == SQUARE_TYPE::ENGINE_WEAPON)
+        m_square_type = SQUARE_TYPE::ENGINE_WEAPON_DESACTIVATED;
+}
+void Square::activate_weapon(){
+    if(m_square_type == SQUARE_TYPE::ENGINE_WEAPON_DESACTIVATED)
+        m_square_type = SQUARE_TYPE::ENGINE_WEAPON;
+}
+void Square::activate_motor(){
+    if(m_square_type == SQUARE_TYPE::ENGINE_MOTOR_DESACTIVATED)
+        m_square_type = SQUARE_TYPE::ENGINE_MOTOR;
+
+}
+SQUARE_TYPE Square::get_square_type(){
+    return m_square_type;
+}
+void Square::set_engine(Engine *engine_a){
+    m_engine = engine_a;
+}
+void Square::set_health_pr(float pr){
+    if(pr < 0.f)
+        m_health_pr = 0;
+    else
+        m_health_pr = pr;
+}
+void Square::set_square_type(SQUARE_TYPE square_type){
+    m_square_type = square_type;
 }
 
-int Square::get_depth()
-{
-        return depth;
-}
+//TODO check if this function can modify the engine health also
+int Square::decrease_health(float dammage) {
+    if(m_engine == nullptr || m_square_type == SQUARE_TYPE::NONE || m_square_type == SQUARE_TYPE::ENGINE_PART_DEAD ||
+       m_square_type == SQUARE_TYPE::ENGINE_WEAPON_DEAD || m_square_type == SQUARE_TYPE::ENGINE_MOTOR_DEAD )
+        return 0;
 
-char Square::get_hazard()
-{
-        return hazard;
-}
-
-void Square::set_engine_here(Engine *engine_here_a)
-{
-        this->engine_here = engine_here_a;
-}
-
-void Square::set_engine_death(bool engine_dead_a)
-{
-        this->engine_dead = engine_dead_a;
-}
-
-void Square::set_depth(int nv) //nv = newvalue or nouveau
-{
-        if (nv >= 0 && nv <= 10) //standard range values
-                depth = nv;
-        else
-                nv = 1; //default value
-}
-
-void Square::set_hazard(char nv)
-{
-        switch (nv)
-        {
-        case 'n':
-                hazard = 'n'; //neutral
-                break;
-        case 'c':
-                hazard = 'c'; //cold
-                break;
-        case 'w':
-                hazard = 'w'; //wind
-                break;
-        case 't':
-                hazard = 't'; //thunder
-                break;
-
-        default:
-                hazard = 'n'; //nothing found -> neutral
-                break;
+    m_engine->take_a_hit(dammage);
+    if(m_health_pr > dammage){
+        m_health_pr -=dammage;
+    }
+    else{
+        m_health_pr = 0;
+        if(m_square_type == SQUARE_TYPE::ENGINE_MOTOR || m_square_type == SQUARE_TYPE::ENGINE_MOTOR_DESACTIVATED){
+            m_engine->kill_motor();
+            m_square_type = SQUARE_TYPE ::ENGINE_MOTOR_DEAD;
         }
+        else if(m_square_type == SQUARE_TYPE::ENGINE_WEAPON || m_square_type == SQUARE_TYPE::ENGINE_WEAPON_DESACTIVATED){
+            m_engine->kill_weapon();
+            m_square_type = SQUARE_TYPE::ENGINE_WEAPON_DEAD;
+        }else{
+            m_square_type = SQUARE_TYPE ::ENGINE_PART_DEAD;
+        }
+    }
+    return 1;
 }
 
-void Square::get_infos()
-{
-        cout << "depth : " << get_depth() << '\n';
-        cout << "hazard : " << get_hazard() << '\n';
-        //cout << "engine : " << get_engine_here() << '\n';
+//TODO
+int Square::increase_health(float care){
+    if(m_engine == nullptr || m_square_type == SQUARE_TYPE::NONE)
+        return 0;
+    m_engine->take_care(care);
+    m_health_pr +=care;
+    if(m_health_pr == 0){
+        if(m_square_type == SQUARE_TYPE::ENGINE_PART_DEAD)
+            m_square_type = SQUARE_TYPE::ENGINE_PART;
+        else if(m_square_type == SQUARE_TYPE::ENGINE_MOTOR_DEAD){
+            m_square_type = SQUARE_TYPE ::ENGINE_MOTOR;
+            m_engine->activate_motor();
+        }
+        else if(m_square_type == SQUARE_TYPE::ENGINE_WEAPON_DEAD){
+            m_square_type = SQUARE_TYPE::ENGINE_WEAPON;
+            m_engine->activate_weapon();
+        }
+    }
+
+    return 1;
 }
-
-/*
-int main()
-{
-        Square *s1 = new Square();
-
-        cout << s1->get_depth() << '\n';
-        cout << s1->get_hazard() << '\n';
-
-        s1->set_depth(52);
-        cout << s1->get_depth() << '\n';
-
-        s1->set_depth(10);
-        cout << s1->get_depth() << '\n';
-
-        s1->set_hazard('w');
-
-        cout << s1->get_hazard() << '\n';
-
-        cout << "Running fine" << '\n';
-
-        delete(s1);
-}
-*/
