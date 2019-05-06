492570bae46679d8f051cfd6e5b4f5124a0b8ffc
Alexandre MEYER <alexandre.meyer3@etu.unistra.fr>
2019-05-06 23:41:46 +0200

    Run.sh corrigé

--- battle_sheep/run.sh
+++ battle_sheep/run.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-[ $# -eq 1 ] && { type=$1 ; } || { type="debug" ; }
 
 
 cd $(dirname $( realpath "$0" ) )
 
 # Creating build directory if needed
-[ -d "build" ] || mkdir build
+[ -d "build" ] || ./build.sh
 cd build
-valgrind --track-origins=yes --leak-check=full ./server >server.out 2>servererr.out & valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client1.out 2>client1err.out & valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client2.out 2>client2err.out & valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client3.out 2>client3err.out & valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client4.out 2>client4err.out && cd ..
-
+valgrind --track-origins=yes --leak-check=full ./server >server.out 2>servererr.out &
+valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client1.out 2>client1err.out &
+valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client2.out 2>client2err.out &
+valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client3.out 2>client3err.out &
+valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client4.out 2>client4err.out
+wait
+wait
+wait
+wait
