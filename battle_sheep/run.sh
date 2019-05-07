f31922d0b90c4c0f93a4ccecf1469bb17b4a8cef
Alexandre MEYER <alexandre.meyer3@etu.unistra.fr>
2019-05-07 14:54:29 +0200

    Retrait de valgrind de run.sh

--- battle_sheep/run.sh
+++ battle_sheep/run.sh
@@ -7,11 +7,11 @@ cd $(dirname $( realpath "$0" ) )
 # Creating build directory if needed
 [ -d "build" ] || ./build.sh
 cd build
-valgrind --track-origins=yes --leak-check=full ./server >server.out 2>servererr.out &
-valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client1.out 2>client1err.out &
-valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client2.out 2>client2err.out &
-valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client3.out 2>client3err.out &
-valgrind --track-origins=yes --leak-check=full ./client localhost 8080 >client4.out 2>client4err.out
+./server >server.out 2>servererr.out &
+./client localhost >client1.out 2>client1err.out &
+./client localhost >client2.out 2>client2err.out &
+./client localhost >client3.out 2>client3err.out &
+./client localhost >client4.out 2>client4err.out
 wait
 wait
 wait
