b5617881f830e9047537e8ac6728898c0e22fbce
Alexandre MEYER <alexandre.meyer3@etu.unistra.fr>
2019-05-07 14:55:17 +0200

    Modification de run.sh

--- battle_sheep/run.sh
+++ battle_sheep/run.sh
@@ -7,11 +7,11 @@ cd $(dirname $( realpath "$0" ) )
 # Creating build directory if needed
 [ -d "build" ] || ./build.sh
 cd build
-./server >server.out 2>servererr.out &
 ./client localhost >client1.out 2>client1err.out &
 ./client localhost >client2.out 2>client2err.out &
 ./client localhost >client3.out 2>client3err.out &
-./client localhost >client4.out 2>client4err.out
+./client localhost >client4.out 2>client4err.out &
+./server >server.out 2>servererr.out
 wait
 wait
 wait
